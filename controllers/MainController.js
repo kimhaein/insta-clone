0b1ecccef0aace934a0dd24271bc8fd6de980c28
rnhappysmile <rnhappysmile@gmail.com>
2020-01-12 15:52:13 +0900

    윈도우 서버에서 file.path가 "\"로 업로드 되는 문제 해결 "\" 를 "/"로 변환 하는 기능, 추후에 사용하는 곳이 더 발생하면 모듈화 진행 필요

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -55,6 +55,7 @@ export const getJoin = async(req, res) => {
         body: { nickname, email, password },
         file
     } = req
+
     usersModel.selectFindUserByEmail(email)
     .then(([row])=>{
         if(row[0].total > 0){
@@ -63,6 +64,14 @@ export const getJoin = async(req, res) => {
         } else {
             // 비밀번호 해시화 
             const hashPassword = crypto.createHash('sha512').update(`${email}@${password}`).digest('base64');
+            
+            // 윈도우 서버에서 file.path가 "\"로 업로드 되는 문제 해결
+            // "\" 를 "/"로 변환 하는 기능, 추후에 사용하는 곳이 더 발생하면 모듈화 진행 필요
+            let replaceCount = file.path.match(/\\/g).length;
+            for(let count = 0;replaceCount > count; count++){
+                file.path = file.path.replace('\\', '/');
+            }
+
             usersModel.insertUser(nickname, email, hashPassword, file.path)
             .then((result) => {
                 // 회원가입 완료시 -> 로그인
