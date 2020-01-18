4cd98861d5b79a7f16ba115cc19a048d98a6efd9
Mengkko <myungwoo@mengkko.dev>
2020-01-18 11:17:58 +0900

    home화면 게시글 profile img 추가

--- models/contentModel.js
+++ models/contentModel.js
@@ -2,7 +2,20 @@ import dbConfig from './dbConfig'
 
 export default {
     home() {
-        const sql = 'select * from content order by idx desc'
+        const sql = `select 
+        idx,
+        fileUrl,
+        creator,
+        creator_email,
+        title,
+        value,
+        regdate,
+        ( select profile_img 
+          from users 
+          where email = creator_email) as profile_img
+       from
+        content 
+       order by idx desc`
         return dbConfig.dbConnect
             .promise()
             .query(sql)
